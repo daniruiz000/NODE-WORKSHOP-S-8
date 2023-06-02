fef25e05b912de4ecfd966c801404db6dae840e9
Daniel Ruiz <danielruimar@gmail.com>
2023-06-02 18:03:31 +0200

    Added index.ts and middlewares and connection data

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,61 @@
-console.log("hola")
-console.log("hola")
+import express from "express";
+import cors from "cors";
+
+import {
+  type Request,
+  type Response,
+} from "express";
+
+// import { techCompaniesRouter } from "./routes/techCompaniesRouter.routes";
+// import { studentRouter } from "./routes/student.routes";
+// import { courseRouter } from "./routes/course.routes";
+
+import { AppDataSource } from "./database/typeorm-datasource";
+
+import { infoReq } from "./middlewares/infoReq.middleware";
+import { checkError } from "./middlewares/error.middleware";
+
+const main = async (): Promise<void> => {
+  // Conexión a la BBDD
+
+  const dataSource = await AppDataSource.initialize()
+
+  // Configuración del server
+  const PORT = 3000;
+  const app = express();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+    })
+  );
+
+  // Rutas
+  const router = express.Router();
+  router.get("/", (req: Request, res: Response) => {
+    res.send(`<h3>Esta es la home de nuestra API.</h3>
+    <p> Estamos utilizando la BBDD TypeORM de ${dataSource.options.database as string}.</p>`);
+  });
+  router.get("*", (req: Request, res: Response) => {
+    res.status(404).send("Lo sentimos :( No hemos encontrado la página solicitada.");
+  });
+
+  // Middleware previo de Info de la req.
+  app.use(infoReq);
+
+  // Usamos las rutas
+  //   app.use("/student", studentRouter);
+  //   app.use("/course", courseRouter);
+  //   app.use("/tech", techCompaniesRouter);
+  app.use("/", router);
+
+  // Middleware de gestión de los Errores.
+  app.use(checkError);
+
+  app.listen(PORT, () => {
+    console.log(`Server levantado en el puerto ${PORT}`);
+  });
+};
+
+void main(); // Si queremos que se espere a que acabe await si da igual void
