c7e89f3a8bff66850aefb8dd97b55a389800921f
Daniel Ruiz <danielruimar@gmail.com>
2023-06-02 19:34:12 +0200

    Added model, router and middlewares

---
+++ src/models/User.ts
@@ -0,0 +1,50 @@
+import {
+  Entity, // Para hacer entidades
+  PrimaryGeneratedColumn, // Para crear una columna id y autogenerada
+  Column, // Para crear columnas
+  OneToMany,
+} from "typeorm"
+
+import { Booking } from "./Booking"
+
+enum treatmentEnum {
+  SR = "Sr.",
+  SRA = "Sra.",
+}
+
+@Entity()
+export class User {
+  toObject(): any {
+    throw new Error("Method not implemented.")
+  }
+
+  @PrimaryGeneratedColumn()
+    id: number
+
+  @Column()
+    firstName: string
+
+  @Column()
+    lastName: string
+
+  @Column({ select: false })
+    password: string
+
+  @Column({ unique: true })
+    email: string
+
+  @Column()
+    dni: string
+
+  @Column()
+    nacionality: string
+
+  @Column()
+    birth_date: string
+
+  @Column()
+    treatment: treatmentEnum
+
+  @OneToMany(type => Booking, booking => booking.user, { cascade: true })
+    bookings: Booking[];
+}
